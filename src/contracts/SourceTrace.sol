78855a2bb0552d95c6208780c47f74043afd7694
Omkar Phansopkar <omkarphansopkar@gmail.com>
2023-03-25 11:03:23 +0530

    Fixed contract & redeployed with latest params
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- src/contracts/SourceTrace.sol
+++ src/contracts/SourceTrace.sol
@@ -1,18 +1,42 @@
 pragma solidity >=0.7.0 <0.9.0;
 pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 // SPDX-License-Identifier: MIT
+
+function toString(bytes memory data) pure returns (string memory) {
+    bytes memory alphabet = "0123456789abcdef";
+
+    bytes memory str = new bytes(2 + data.length * 2);
+    str[0] = "0";
+    str[1] = "x";
+    for (uint256 i = 0; i < data.length; i++) {
+        str[2 + i * 2] = alphabet[uint256(uint8(data[i] >> 4))];
+        str[3 + i * 2] = alphabet[uint256(uint8(data[i] & 0x0f))];
+    }
+    return string(str);
+}
+
+// cast address to string
+function addressToString(address account) pure returns (string memory) {
+    return toString(abi.encodePacked(account));
+}
 
 contract SourceTrace {
     struct Producer {
         address id;
         string name;
+        string phone;
+        string reg_no;
         string location;
     }
 
     struct Warehouse {
         address id;
         string name;
+        string phone;
+        string reg_no;
         string location;
     }
 
@@ -21,14 +45,19 @@ contract SourceTrace {
         address producer;
         string name;
         string price;
+        string[] params;
+        uint256[] minValues;
+        uint256[] maxValues;
     }
 
     struct Checkpoint {
         uint256 inTime;
         uint256 outTime;
         Warehouse warehouse;
-        uint256 temperature;
-        uint256 humidity;
+        uint256 in_temperature;
+        uint256 in_humidity;
+        uint256 out_temperature;
+        uint256 out_humidity;
     }
 
     struct ProductLot {
@@ -39,8 +68,13 @@ contract SourceTrace {
         uint256 createdAt;
         string sourceFactoryName;
         string sourceFactoryLocation;
-        Checkpoint[] checkpoints;
+        // Checkpoint[] checkpoints;
     }
+
+    // Producer_productlotIndex => Checkpoint[]
+    // eg. 0xab32..2c_0 => [Checkpoint{..}, Checkpoint{..}, ..., Checkpoint{..}]
+    // eg. 0xab32..2c_2 => [Checkpoint{..}, Checkpoint{..}, ..., Checkpoint{..}]
+    mapping(string => Checkpoint[]) checkpoints;
 
     // 0x923abc...52 => Producer {...} | Warehouse {...}
     mapping(address => Producer) producers;
@@ -131,7 +165,36 @@ contract SourceTrace {
         return productLots[_producer_address][_product_lot_id];
     }
 
-    function createProducer(string memory _name, string memory _location)
+    function getProductLotCheckpoints(
+        address _producer_address,
+        uint256 _product_lot_id
+    ) public view returns (Checkpoint[] memory) {
+        return
+            checkpoints[
+                string.concat(
+                    addressToString(_producer_address),
+                    "_",
+                    Strings.toString(_product_lot_id)
+                )
+            ];
+    }
+
+    function getProductLotCheckpoint(
+        address _producer_address,
+        uint256 _product_lot_id,
+        uint256 _idx
+    ) public view returns (Checkpoint memory) {
+        return
+            checkpoints[
+                string.concat(
+                    addressToString(_producer_address),
+                    "_",
+                    Strings.toString(_product_lot_id)
+                )
+            ][_idx];
+    }
+
+    function createProducer(string memory _name, string memory _phone, string memory _reg_no, string memory _location)
         public
     {
         // Ensure a producer with the same address does not already exist
@@ -144,12 +207,14 @@ contract SourceTrace {
         Producer memory newProducer = Producer({
             id: msg.sender,
             name: _name,
+            phone: _phone,
+            reg_no: _reg_no,
             location: _location
         });
         producers[msg.sender] = newProducer;
     }
 
-    function createWarehouse(string memory _name, string memory _location)
+    function createWarehouse(string memory _name, string memory _phone, string memory _reg_no, string memory _location)
         public
     {
         // Ensure a warehouse with the same address does not already exist
@@ -162,111 +227,175 @@ contract SourceTrace {
         Warehouse memory newWarehouse = Warehouse({
             id: msg.sender,
             name: _name,
+            phone: _phone,
+            reg_no: _reg_no,
             location: _location
         });
         warehouses[msg.sender] = newWarehouse;
     }
 
-    function inventProduct(string memory _name, string memory _price)
-        public
-        mustBeProducer(msg.sender)
-        returns (uint256)
-    {
+    function inventProduct(
+        string memory _name,
+        string memory _price,
+        string[] memory _params,
+        uint256[] memory _minValues,
+        uint256[] memory _maxValues
+    ) public mustBeProducer(msg.sender) returns (uint256) {
         uint256 newProductId = productsInfo[msg.sender].length;
         ProductInfo memory newProductInfo = ProductInfo({
             productId: newProductId,
             producer: msg.sender,
             name: _name,
-            price: _price
+            price: _price,
+            params: _params,
+            minValues: _minValues,
+            maxValues: _maxValues
         });
         productsInfo[msg.sender].push(newProductInfo);
         return newProductId;
     }
 
-    // function test(address sender, uint256 _product_id) public view returns (bool) {
-    //     // 0x0000000000000000000000000000000000000000
-    //     return _product_id < productsInfo[sender].length;
-    //     // return productsInfo[msg.sender][0].producer != 0x0000000000000000000000000000000000000000;
-    // }
     function createProductLot(
-    uint256 _quantity,
-    uint256 _product_id,
-    string memory _source_factory_name,
-    string memory _source_factory_location,
-    uint256 _temperature,
-    uint256 _humidity
-) public mustBeProducer(msg.sender) returns (uint256) {
-    require(
-        _product_id < productsInfo[msg.sender].length,
-        "This product doesn't exist, Invent it first !"
-    );
-    uint256 newProductLotId = productLots[msg.sender].length;
-    uint256 createdAt = block.timestamp;
+        uint256 _quantity,
+        uint256 _product_id,
+        string memory _source_factory_name,
+        string memory _source_factory_location,
+        uint256 _temperature,
+        uint256 _humidity
+    )
+        public
+        mustBeProducer(msg.sender)
+        returns (string memory)
+    // returns (uint256)
+    {
+        require(
+            _product_id < productsInfo[msg.sender].length,
+            "This product doesn't exist, Invent it first !"
+        );
+        uint256 newProductLotId = productLots[msg.sender].length;
+        uint256 createdAt = block.timestamp;
 
-    ProductLot memory newProductLot = ProductLot({
-        productId: _product_id,
-        productLotId: newProductLotId,
-        producerAddress: msg.sender,
-        quantity: _quantity,
-        createdAt: createdAt,
-        sourceFactoryName: _source_factory_name,
-        sourceFactoryLocation: _source_factory_location,
-        checkpoints: new Checkpoint[](0)
-    });
+        ProductLot memory newProductLot = ProductLot({
+            productId: _product_id,
+            productLotId: newProductLotId,
+            producerAddress: msg.sender,
+            quantity: _quantity,
+            createdAt: createdAt,
+            sourceFactoryName: _source_factory_name,
+            sourceFactoryLocation: _source_factory_location
+        });
+        productLots[msg.sender].push(newProductLot);
 
-    productLots[msg.sender].push(newProductLot);
+        // add the check-in checkpoint
+        Checkpoint memory checkIn = Checkpoint({
+            inTime: block.timestamp,
+            outTime: 0,
+            warehouse: warehouses[msg.sender], // Empty obj, coz producer isn't always a warehouse
+            in_temperature: _temperature,
+            in_humidity: _humidity,
+            out_temperature: 0,
+            out_humidity: 0
+        });
 
-    // add the check-in checkpoint
-    Checkpoint memory checkIn = Checkpoint({
-        inTime: block.timestamp,
-        outTime: 0,
-        warehouse: warehouses[msg.sender],
-        temperature: _temperature,
-        humidity: _humidity
-    });
+        checkpoints[
+            string.concat(
+                addressToString(msg.sender),
+                "_",
+                Strings.toString(newProductLotId)
+            )
+        ].push(checkIn);
 
-    productLots[msg.sender][newProductLotId].checkpoints.push(checkIn);
-
-    return newProductLotId;
-}
+        return
+            string.concat(
+                addressToString(msg.sender),
+                "_",
+                Strings.toString(newProductLotId)
+            );
+        // return newProductLotId;
+    }
 
     function createCheckIn(
         address _producer_address,
         uint256 _product_lot_id,
-        address _warehouseAddress
+        uint256 _temperature,
+        uint256 _humidity
     ) public mustBeWarehouse(msg.sender) {
         // retrieve the warehouse by its address
-        Warehouse memory warehouse = warehouses[_warehouseAddress];
+        Warehouse memory warehouse = warehouses[msg.sender];
 
         // retrieve the product lot
-        ProductLot storage productLot = productLots[_producer_address][_product_lot_id];
+        // ProductLot storage productLot = productLots[_producer_address][_product_lot_id];
 
         // add a checkpoint to the product lot
-        uint256 targetCheckpointIndex = productLot.checkpoints.length;
-        productLot.checkpoints.push();
+        Checkpoint[] storage targetCheckpoints = checkpoints[
+            string.concat(
+                addressToString(_producer_address),
+                "_",
+                Strings.toString(_product_lot_id)
+            )
+        ];
+        uint256 targetCheckpointIndex = targetCheckpoints.length;
+        targetCheckpoints.push();
 
         // update values of newly added product lot
-        Checkpoint storage newCheckpoint = productLot.checkpoints[targetCheckpointIndex];
+        Checkpoint storage newCheckpoint = targetCheckpoints[
+            targetCheckpointIndex
+        ];
         newCheckpoint.inTime = block.timestamp;
         newCheckpoint.outTime = 0;
         newCheckpoint.warehouse = warehouse;
+        newCheckpoint.in_temperature = _temperature;
+        newCheckpoint.in_humidity = _humidity;
+        newCheckpoint.out_temperature = 0;
+        newCheckpoint.out_humidity = 0;
     }
 
     function createCheckOut(
         address _producer_address,
-        uint256 _product_lot_id
-    ) public mustBeWarehouse(msg.sender) {
+        uint256 _product_lot_id,
+        uint256 _temperature,
+        uint256 _humidity
+    ) public {
         // retrieve the product lot
-        ProductLot storage productLot = productLots[_producer_address][_product_lot_id];
+        // ProductLot storage productLot = productLots[_producer_address][_product_lot_id];
 
         // retrieve the checkpoint
-        uint256 _checkpointIndex = productLot.checkpoints.length-1;
-        Checkpoint storage checkpoint = productLot.checkpoints[_checkpointIndex];
+        Checkpoint[] storage targetCheckpoints = checkpoints[
+            string.concat(
+                addressToString(_producer_address),
+                "_",
+                Strings.toString(_product_lot_id)
+            )
+        ];
+        uint256 _checkpointIndex = targetCheckpoints.length - 1;
+        Checkpoint storage checkpoint = targetCheckpoints[_checkpointIndex];
 
-        // Ensure the checkpoint has not already been checked out
-        require(checkpoint.outTime == 0, "This checkpoint has already been checked out");
+        if (_checkpointIndex == 0) {
+            // 0th Checkout from producer only
+            require(
+                _producer_address == msg.sender,
+                "Only original producer can checkout from factory !"
+            );
+        } else {
+            require(
+                warehouses[msg.sender].id !=
+                    0x0000000000000000000000000000000000000000,
+                "Warehouse doesn't exist with this address"
+            );
+            require(
+                checkpoint.warehouse.id == msg.sender,
+                "Can't checkout from someone else's checkpoint !!"
+            );
+        }
 
-        // set the outTime for the checkpoint
+        require(
+            checkpoint.outTime == 0,
+            "This checkpoint has already been checked out"
+        );
+
+        // set the out params for the checkpoint
         checkpoint.outTime = block.timestamp;
+        checkpoint.out_temperature = _temperature;
+        checkpoint.out_humidity = _humidity;
     }
 }
--- src/contracts/deploymentDetails.js
+++ src/contracts/deploymentDetails.js
@@ -1,5 +1,5 @@
-export const contractDeploymentTx = "0x0136916d5b120f5773a7a25320079223c3321495b283fd7432f8c3f7ccdbbdc8";
-export const contractAddress = "0x6E35Eff4b9ca47bfA50ea0B391DCD22B86f00564";
+export const contractDeploymentTx = "0x331178f6c6637b7ef83336705f04eb4a5835fdf4663cb7b564ecd20bba038815";
+export const contractAddress = "0xD362314f3d86477Fe29925D6ae665EC151a4C11E";
 
 export const chainExplorerBaseAddress = "https://mumbai.polygonscan.com";
 export const contractDeploymentTxLink = `${chainExplorerBaseAddress}/tx/${contractDeploymentTx}`;

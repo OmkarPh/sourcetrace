d061cea3b4fdbcd364d6f56d7ebbb125eacd453e
nishant <nishantd28satere@gmail.com>
2023-03-26 11:10:47 +0530

    Created registration pages

---
+++ src/pages/WarehouseRegister.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+// import React, { useState} from "react";
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import Warehouse from './Warehouse.svg'
+
+
+function WarehouseRegister(){
+
+    const [info, setInfo] = useState({
+        name: '',
+        phoneno: '',
+        regno: '',
+        address: '',
+    })
+
+    const inputEvent = (event) => {
+        console.log(event.target.value); 
+
+        const value = event.target.value;
+        const name = event.target.value;
+
+        setInfo({...info, [name]: value});
+        // setInfo((prev) => {
+        //     // console.log(prev)
+        //     return {...prev, [name]: value};
+        // })
+    }
+
+    const onSubmits = (event) =>
+    {
+        event.preventDefault();
+        // console.log(setInfo.name)
+        // console.log(setInfo.name)
+    }
+
+    return (
+        <div>
+        <label className="Sourcetraceheading">Source Trace</label>
+        <label className="Projecttitle">Supply chain management software</label>
+        <img src={Warehouse} className="Producer-logo" alt="Warehouselogo" />
+        
+        <form onSubmit={onSubmits}>
+        {/* NAME */}
+        <label htmlFor="name" className="Name">Name: </label>
+        <TextField className="Namebox" onChange={inputEvent}  id="Name" label="Name" variant="filled" autoComplete="off"/>
+
+
+        {/* Phone no */}
+        <label className="Phoneno">Phone no: </label>
+        <TextField className="Phonenobox" onChange={inputEvent} id="Phoneno" label="Phone no" variant="filled" autoComplete="off"/>
+
+
+        {/* Registration No */}
+        <label className="Regno">Registeration No: </label>
+        <TextField className="Regnobox" onChange={inputEvent} id="Regno" label="Registeration No" variant="filled" autoComplete="off"/>
+
+
+        {/* Address */}
+        <label className="Address">Address: </label>
+        <TextField className="Addressbox" onChange={inputEvent}  id="Address" label="Address" variant="filled" autoComplete="off"/>
+       
+
+        {/* Button */}
+        <Button className="Confirmbtn" type="submit" variant="contained">CONFIRM</Button>
+        </form>
+        </div>
+    )
+}
+
+export default WarehouseRegister

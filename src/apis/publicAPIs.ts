4bb0d9cf089e35cf2cc1b3225d5b01f65d012493
Omkar Phansopkar <omkarphansopkar@gmail.com>
2023-04-23 15:51:16 +0530

    public apis
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

---
+++ src/apis/publicAPIs.ts
@@ -0,0 +1,146 @@
+import {
+  ProductLotWithCheckpoints,
+  Warehouse,
+} from "./../components/Dashboard/productTypes";
+import {
+  ProductLot,
+  ProductInfo,
+  Checkpoint,
+} from "../components/Dashboard/productTypes";
+import { CallerFactory, SenderFactory } from "./factory";
+import SourceTraceContract from "./SourceTraceContract";
+import {
+  parseRejectionMessage,
+  productIdentifierToDetails,
+} from "../utils/general";
+
+
+
+export const GetProducer = CallerFactory(
+  SourceTraceContract,
+  "getProducer",
+  false
+);
+export const GetWarehouse = CallerFactory(
+  SourceTraceContract,
+  "getWarehouse",
+  false
+);
+export const GetAllProductsInfo = CallerFactory(
+  SourceTraceContract,
+  "getAllProductsInfo",
+  false
+);
+export const GetProductInfo = CallerFactory(
+  SourceTraceContract,
+  "getProductInfo",
+  false
+);
+export const GetAllProductLots = CallerFactory(
+  SourceTraceContract,
+  "getAllProductLots",
+  false
+);
+export const GetProductLot = CallerFactory(
+  SourceTraceContract,
+  "getProductLot",
+  false
+);
+export const GetProductLotCheckpoints = CallerFactory(
+  SourceTraceContract,
+  "getProductLotCheckpoints",
+  false
+);
+export const GetProductLotCheckpoint = CallerFactory(
+  SourceTraceContract,
+  "getProductLotCheckpoint",
+  false
+);
+
+export const CreateProducerFn = SenderFactory(
+  SourceTraceContract,
+  "createProducer",
+  true
+);
+export const CreateWarehouseFn = SenderFactory(
+  SourceTraceContract,
+  "createWarehouse",
+  true
+);
+
+export const InventProduct = SenderFactory(
+  SourceTraceContract,
+  "inventProduct",
+  true
+);
+export const CreateProductLot = SenderFactory(
+  SourceTraceContract,
+  "createProductLot",
+  true
+);
+export const CreateCheckIn = SenderFactory(
+  SourceTraceContract,
+  "createCheckIn",
+  true
+);
+export const CreateCheckOut = SenderFactory(
+  SourceTraceContract,
+  "createCheckOut",
+  true
+);
+export const Reject = SenderFactory(SourceTraceContract, "reject", true);
+export const AddTruck = SenderFactory(SourceTraceContract, "addTruck", false);
+// export const  = SenderFactory(SourceTraceContract, '', false);
+
+
+
+// Public API functions
+export const GetWarehouseProductLots = CallerFactory(
+  SourceTraceContract,
+  "getWarehouseProductLots",
+  false
+);
+export const GetWarehouseProductLotsWithCheckpoints = async (
+  warehouse: string
+) => {
+  const lots = (await GetWarehouseProductLots(warehouse)) as string[];
+  const productLots: ProductLotWithCheckpoints[] = [];
+  for (const lot of lots) {
+    const { producer, id } = productIdentifierToDetails(lot);
+    productLots.push(await GetProductLotWithCheckpoints(producer, id));
+    console.log("Warehouse lot", await GetProductLotCheckpoints(producer, id));
+  }
+  return productLots;
+};
+export const GetProductLotWithCheckpoints = async (
+  producer: string,
+  id: number
+) => {
+  const lot = (await GetProductLot(producer, id)) as ProductLot;
+  const productInfo = (await GetProductInfo(
+    producer,
+    lot.productId
+  )) as ProductInfo;
+  const checkpoints = (await GetProductLotCheckpoints(
+    producer,
+    lot.productLotId
+  )) as Checkpoint[];
+
+  const rejection = lot.rejected
+    ? parseRejectionMessage(lot.rejectedMessage)
+    : null;
+
+  return {
+    ...lot,
+    rejection: rejection
+      ? {
+          ...rejection,
+          rejectedByWarehouse: (await GetWarehouse(
+            rejection.rejectedByAddress
+          )) as Warehouse,
+        }
+      : null,
+    productInfo,
+    checkpoints,
+  };
+};

09c551d7ec901467e844cfc2a25b0d6f977e35c8
nishant <nishantd28satere@gmail.com>
2023-04-23 23:14:54 +0530

    udpated api

--- src/apis/publicAPIs.ts
+++ src/apis/publicAPIs.ts
@@ -13,6 +13,7 @@ import {
   parseRejectionMessage,
   productIdentifierToDetails,
 } from "../utils/general";
+import exp from "constants";
 
 
 
@@ -21,11 +22,21 @@ export const GetProducer = CallerFactory(
   "getProducer",
   false
 );
+
+export function getProducer(producerName: string, producerPhone:number, producerRegno:number, producerLocation:string){
+  return GetProducer(producerName, producerPhone, producerRegno, producerLocation);
+}
+
 export const GetWarehouse = CallerFactory(
   SourceTraceContract,
   "getWarehouse",
   false
 );
+
+export function getWarehouse(warehouseName: string, warehousePhone:number, warehouseRegno:number, warehouseLocation:string){
+  return GetWarehouse(warehouseName, warehousePhone, warehouseRegno, warehouseLocation);
+}
+
 export const GetAllProductsInfo = CallerFactory(
   SourceTraceContract,
   "getAllProductsInfo",
@@ -36,6 +47,11 @@ export const GetProductInfo = CallerFactory(
   "getProductInfo",
   false
 );
+
+export function getProductsInfo(productName: string, productId:number, productloc: string, productTemp:number, productHum:number){
+  return GetProductInfo(productName, productId, productTemp, productHum);
+}
+
 export const GetAllProductLots = CallerFactory(
   SourceTraceContract,
   "getAllProductLots",
@@ -46,6 +62,11 @@ export const GetProductLot = CallerFactory(
   "getProductLot",
   false
 );
+
+export function getAllProductsInfo(productName: string, quantity: number, productId:number, productloc: string, productTemp:number, productHum:number){
+  return GetProductLot(productName, quantity, productId, productTemp, productHum);
+}
+
 export const GetProductLotCheckpoints = CallerFactory(
   SourceTraceContract,
   "getProductLotCheckpoints",
@@ -62,32 +83,60 @@ export const CreateProducerFn = SenderFactory(
   "createProducer",
   true
 );
+
+export function CrateProducer(producerName: string, producerPhone:number, producerRegno:number, producerLocation:string){
+  return CreateProducerFn(producerName, producerPhone, producerRegno, producerLocation);
+}
+
 export const CreateWarehouseFn = SenderFactory(
   SourceTraceContract,
   "createWarehouse",
   true
 );
 
+export function CreateWarehouse(warehouseName: string, warehousePhone:number, warehouseRegno:number, warehouseLocation:string){
+  return CreateWarehouseFn(warehouseName, warehousePhone, warehouseRegno, warehouseLocation);
+}
+
 export const InventProduct = SenderFactory(
   SourceTraceContract,
   "inventProduct",
   true
 );
+
+export function Invent(name:any, price: number, imgURL: string, isPerishable:any, params: string[], minValues: number[], maxValues: number[]){
+  return InventProduct(name, price, imgURL, isPerishable, params, minValues, maxValues)
+}
+
 export const CreateProductLot = SenderFactory(
   SourceTraceContract,
   "createProductLot",
   true
 );
+
+export function Productlot( productName: string, quantity: number, productId:number, productloc: string, productTemp:number, productHum:number){
+  return CreateProductLot(productName, quantity, productId, productTemp, productHum);
+}
+
 export const CreateCheckIn = SenderFactory(
   SourceTraceContract,
   "createCheckIn",
   true
 );
+
+export function CheckIn(myAdrress: string, productLotID: number, productTemp: number, productHum: number){
+  return CreateCheckIn(myAdrress, productLotID, productTemp, productHum);
+}
+
 export const CreateCheckOut = SenderFactory(
   SourceTraceContract,
   "createCheckOut",
   true
 );
+
+export function CheckOut(myAdrress: string, productLotID: number, productTemp: number, productHum: number){
+  return CreateCheckOut(myAdrress, productLotID, productTemp, productHum);
+}
 export const Reject = SenderFactory(SourceTraceContract, "reject", true);
 export const AddTruck = SenderFactory(SourceTraceContract, "addTruck", false);
 // export const  = SenderFactory(SourceTraceContract, '', false);
